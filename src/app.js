460647fda85d8c6b1940889943cf24989faaef72
Katy Morris <katymorris@Katys-MBP.home>
2016-10-21 17:34:30 -0700

    added test route

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 'use strict';
 
 var express = require('express');
+var posts = require('./mock/posts.json')
 
 var app = express();
 
-app.get('/', function(request, response) {
-	response.send("I Love Treehouse");
+app.get('/', function(req, res) {
+	res.send("<h1>I Am In Love Treehouse</h1>");
 })
 
-app.listen(3000);
+app.get('/blog', function(req, res) {
+	console.log('working')
+	res.send(posts);
+})
+
+app.listen(3000, function() {
+	console.log("The frontend server is running on port 3000")
+});
